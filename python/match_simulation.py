e08690d56f023ee0bb6321244ebab79a04f02016
Reznikov Roman <romanreznikov2002@yandex.ru>
2020-03-16 16:57:45 +0300

    Simulate match and 'bartlett' MMR

--- python/match_simulation.py
+++ python/match_simulation.py
@@ -19,10 +19,11 @@ class Player(object):
         self.curr_rank = current_rank
         self.prev_rank = prev_rank
         self.bartlett_rank = rank_formula(self.prev_rank, self.curr_rank)
+        self.team_role = None
 
     def __str__(self):
         return f"""Nickname: {self.discord_nick}, R6Tab ID: {self.tab_id};
-Current MMR: {self.curr_rank}, Previous MMR: {self.prev_rank}, Average MMR: {self.bartlett_rank}\n"""
+Current MMR: {self.curr_rank}, Previous MMR: {self.prev_rank}, Average MMR: {self.bartlett_rank}, Team: {self.team_role}\n"""
 
 # =========================================
 
@@ -65,13 +66,32 @@ for player in sorted_lobby:
 # ==============================
 
 for player_ind in range(len(sorted_lobby)):
-    teamA.append(sorted_lobby[player_ind]) if player_ind % 2 == 0 else teamB.append(sorted_lobby[player_ind])
+    if player_ind % 2 == 0:
+        sorted_lobby[player_ind].team_role = "role_Team_A_"
+        teamA.append(sorted_lobby[player_ind])
+    else:
+        sorted_lobby[player_ind].team_role = "role_Team_B_"
+        teamB.append(sorted_lobby[player_ind])
+    # teamA.append(sorted_lobby[player_ind]) if player_ind % 2 == 0 else teamB.append(sorted_lobby[player_ind])
 
-# ======Test=======
+# ======-Test-1-========
+# print()
+# for playerA, playerB in zip(teamA, teamB):
+#     print(playerA.bartlett_rank, end=" ")
+#     print(playerB.bartlett_rank)
+
+# ======-Test-2-========
 print()
-for playerA, playerB in zip(teamA, teamB):
-    print(playerA.bartlett_rank, end=" ")
-    print(playerB.bartlett_rank)
+
+print("Team A: ")
+for player in teamA:
+    print(player)
+
+print()
+print("Team B: ")
+for player in teamB:
+    print(player)
+
 # =======================================================
 
 
